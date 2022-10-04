a40d4d3830404a6c43ce82f1da7f83c90b827828
kirilgravis <68824674+kirilgravis@users.noreply.github.com>
2022-10-04 18:45:10 +0300

    more methods

--- main.py
+++ main.py
@@ -1,128 +1,74 @@
-from collections import deque
-import random
-import math
 import gym
 import numpy as np
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import math
 
 
-class DQN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.fc1 = nn.Linear(4, 24)
-        self.fc2 = nn.Linear(24, 48)
-        self.fc3 = nn.Linear(48, 2)
-
-
-def forward(self, x):
-    x = self.fc1(x)
-    x = F.relu(x)
-    x = self.fc2(x)
-    x = F.relu(x)
-    x = self.fc3(x)
-    return x
-
-
-class DQNCartPoleSolver:
-    def __init__(self, n_episodes=1000, n_win_ticks=195, max_env_steps=None, gamma=1.0, epsilon=1.0, epsilon_min=0.01,
-                 epsilon_log_decay=0.995, alpha=0.01, alpha_decay=0.01, batch_size=64, monitor=False, quiet=False):
-        self.memory = deque(maxlen=100000)
+class CartPoleQAgent():
+    def __init__(self, buckets=(1, 1, 6, 12), num_episodes=1000, min_lr=0.1, min_explore=0.1, discount=1.0, decay=25):
+        self.buckets = buckets
+        self.num_episodes = num_episodes
+        self.min_lr = min_lr
+        self.min_explore = min_explore
+        self.discount = discount
+        self.decay = decay
         self.env = gym.make('CartPole-v0')
-        if monitor: self.env = gym.wrappers.Monitor(self.env, '../data/cartpole-1', force=True)
-        self.gamma = gamma
-        self.epsilon = epsilon
-        self.epsilon_min = epsilon_min
-        self.epsilon_decay = epsilon_log_decay
-        self.alpha = alpha
-        self.alpha_decay = alpha_decay
-        self.n_episodes = n_episodes
-        self.n_win_ticks = n_win_ticks
-        self.batch_size = batch_size
-        self.quiet = quiet
-        if max_env_steps is not None: self.env._max_episode_steps = max_env_steps
-        # Init model
-        self.dqn = DQN()
-        self.criterion = torch.nn.MSELoss()
-        self.opt = torch.optim.Adam(self.dqn.parameters(), lr=0.01)
+        self.upper_bounds = [self.env.observation_space.high[0], 0.5, self.env.observation_space.high[2], math.radians(50) / 1.]
+        self.lower_bounds = [self.env.observation_space.low[0], -0.5, self.env.observation_space.low[2], -math.radians(50) / 1.]
+        self.Q_table = np.zeros(self.buckets + (self.env.action_space.n,))
 
+    def update_q(self, state, action, reward, new_state):
+        self.Q_table[state][action] += self.lr * (
+                    reward + self.discount * np.max(self.Q_table[new_state]) - self.Q_table[state][action])
 
-    def get_epsilon(self, t):
-        return max(self.epsilon_min, min(self.epsilon, 1.0 - math.log10((t + 1) * self.epsilon_decay)))
+    def get_explore_rate(self, t):
+        return max(self.min_explore, min(1., 1. - math.log10((t + 1) / self.decay)))
 
+    def get_lr(self, t):
+        return max(self.min_lr, min(1., 1. - math.log10((t + 1) / self.decay)))
 
-    def preprocess_state(self, state):
-        return torch.tensor(np.reshape(state, [1, 4]), dtype=torch.float32)
-
-
-    def choose_action(self, state, epsilon):
-        if (np.random.random() <= epsilon):
+    def choose_action(self, state):
+        if (np.random.random() < self.explore_rate):
             return self.env.action_space.sample()
         else:
-            with torch.no_grad():
-                return torch.argmax(self.dqn(state)).numpy()
+            return np.argmax(self.Q_table[state])
 
+    def discretize_state(self, obs):
+        discretized = list()
+        for i in range(len(obs)):
+            scaling = (obs[i] + abs(self.lower_bounds[i])) / (self.upper_bounds[i] - self.lower_bounds[i])
+            new_obs = int(round((self.buckets[i] - 1) * scaling))
+            new_obs = min(self.buckets[i] - 1, max(0, new_obs))
+            discretized.append(new_obs)
+        return tuple(discretized)
 
-    def remember(self, state, action, reward, next_state, done):
-        reward = torch.tensor(reward)
-        self.memory.append((state, action, reward, next_state, done))
-
-
-    def replay(self, batch_size):
-        y_batch, y_target_batch = [], []
-        minibatch = random.sample(self.memory, min(len(self.memory), batch_size))
-        for state, action, reward, next_state, done in minibatch:
-            y = self.dqn(state)
-            y_target = y.clone().detach()
-            with torch.no_grad():
-                y_target[0][action] = reward if done else reward + self.gamma * torch.max(self.dqn(next_state)[0])
-            y_batch.append(y[0])
-            y_target_batch.append(y_target[0])
-
-        y_batch = torch.cat(y_batch)
-        y_target_batch = torch.cat(y_target_batch)
-
-        self.opt.zero_grad()
-        loss = self.criterion(y_batch, y_target_batch)
-        loss.backward()
-        self.opt.step()
-
-        if self.epsilon > self.epsilon_min:
-            self.epsilon *= self.epsilon_decay
+    def train(self):
+        for e in range(self.num_episodes):
+            current_state = self.discretize_state(self.env.reset())
+            self.lr = self.get_lr(e)
+            self.explore_rate = self.get_explore_rate(e)
+            done = False
+            while not done:
+                action = self.choose_action(current_state)
+                obs, reward, done, _ = self.env.step(action)
+                new_state = self.discretize_state(obs)
+                self.update_q(current_state, action, reward, new_state)
+                current_state = new_state
+        print('Finished training!')
 
     def run(self):
-        scores = deque(maxlen=100)
-        for e in range(self.n_episodes):
-            state = self.preprocess_state(self.env.reset())
+        self.env = gym.wrappers.Monitor(self.env, directory='cartpole', force=True)
+        while True:
+            current_state = self.discretize_state(self.env.reset())
             done = False
-            i = 0
             while not done:
-                if e % 100 == 0 and not self.quiet:
-                    self.env.render()
-                action = self.choose_action(state, self.get_epsilon(e))
-                next_state, reward, done, _ = self.env.step(action)
-                next_state = self.preprocess_state(next_state)
-                self.remember(state, action, reward, next_state, done)
-                state = next_state
-                i += 1
-        scores.append(i)
-        mean_score = np.mean(scores)
-        if mean_score >= self.n_win_ticks and e >= 100:
-            if not self.quiet: print('Ran {} episodes. Solved after {} trials ✔'.format(e, e - 100))
-            return e - 100
-        if e % 100 == 0 and not self.quiet:
-            print('[Episode {}] - Mean survival time over last 100 episodes was {} ticks.'.format(e, mean_score))
-
-        self.replay(self.batch_size)
-
-        if not self.quiet:
-            print('Did not solve after {} episodes      😞'.format(e))
-            return e
+                self.env.render()
+                action = self.choose_action(current_state)
+                obs, reward, done, _ = self.env.step(action)
+                new_state = self.discretize_state(obs)
+                current_state = new_state
 
 
-if __name__ == '__main__':
-    agent = DQNCartPoleSolver()
+if __name__ == "__main__":
+    agent = CartPoleQAgent()
+    agent.train()
     agent.run()
-    agent.env.close()
-
